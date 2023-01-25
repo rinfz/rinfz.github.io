ad0bccf3f242afb5b898e4ced127c28f2117c31b
rinfz <mr@b5.re>
2023-01-25 20:08:53 +0000

    Upgrade to astro v2
    Finish styling of photos page
    
    Update some whiskies, reshuffle home page
    
    Changes to layout, make nav toggle-able in mobile mode
    
    Add stuff to songs and whiskies, add a page for songs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import { astroImageTools } from "astro-imagetools";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://b5.re",
-  integrations: [tailwind(), astroImageTools],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
   markdown: {
     shikiConfig: {
-      theme: "poimandres",
-    },
-  },
+      theme: "poimandres"
+    }
+  }
 });
