7871a7aba40939afec7280af7c4ec6eae9514ddc
rinfz <mr@b5.re>
2023-02-02 20:03:41 +0000

    Fix link sorting and change homepage a bit

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,15 @@
 import { z, defineCollection } from "astro:content";
 
+const re = /^0/;
+
 const link = defineCollection({
     schema: z.object({
         title: z.string(),
         url: z.string(),
-        date: z.string().transform(s => new Date(s)),
+        date: z.string().transform(s => {
+            const parts = s.split('/').map(n => parseInt(n.replace(re, '')));
+            return new Date(parts[0], parts[1] - 1, parts[2]);
+        }),
     })
 });
 
